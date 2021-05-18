ad6f8c725ed78543584c7e14269ac98fa5dd3509
nescafe_boy <lestherbualan1718@gmail.com>
2021-05-19 00:23:46 +0800

    navigation complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/landing/navbar/navbar.component';
 
+
 const routes: Routes = [
-  { path: '', component: NavbarComponent }
 ];
 
 @NgModule({
